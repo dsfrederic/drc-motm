53bd052d56192a6aa1518b09ec4073b2bc0ba02a
Frédéric De Smet <fredericdesmet@live.be>
2023-10-03 05:51:43 +0000

    GameContext.js implemented

--- frontend/src/contexts/GameContext.js
+++ frontend/src/contexts/GameContext.js
@@ -1,13 +1,61 @@
-import React, { createContext } from 'react';
-
-const GameContext = createContext();
-
-// create Pet Provider
-export const GameProvider = ({ children }) => {
-    const value = {};
-    return (
-        <GameContext.Provider value={value}>
-            {children}
-        </GameContext.Provider>
-    )
-};
+import React, { createContext, useContext, useEffect, useState } from 'react';
+    import http from '../http';
+    const GameContext = createContext();
+    
+    export const useGameContext = () => {
+        return useContext(GameContext);
+    };
+    
+    export const GameProvider = ({children}) => {
+        const [games, setGames] = useState("");
+        const [nav_value, set_nav_value] = useState("GameList");
+        const [gameId, setGameId] = useState("");
+        
+        // add new pet
+        const createNewGame = async (data) => {
+            await http.post("/api/games", data);
+        };
+        // update a pet entry
+        const updateGame = async (gameId, data) => {
+            await http.put(`/api/games/${gameId}`, data);
+        };
+        // delete a pet entry
+        const deleteGame = async (gameId) => {
+            await http.delete(`/api/games/${gameId}`);
+        };
+        // change navigation value
+        const changeNavValue = (value) => {
+            set_nav_value(value);
+        };
+        // get pet id value
+        const getGameId = (id) => {
+            setGameId(id);
+        };
+    
+        useEffect(()=>{
+            const readAllPets = async () => {
+                const response = await http.get("/api/games");
+                const responseArr = Object.values(response.data.data);
+                setGames(responseArr);
+            };
+            return readAllPets;
+        }, []);
+    
+        const value = {
+            createNewGame,
+            games,
+            updateGame,
+            deleteGame,
+            changeNavValue,
+            nav_value,
+            getGameId,
+            gameId
+        };
+    
+        // context provider
+        return(
+            <GameContext.Provider value={value}>
+                {children}
+            </GameContext.Provider>
+        )
+    }; 
